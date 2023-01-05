d57f0ef505bf566fcfb07b8aef8f58a9b72eb368
Patrick Mazara <patrickmazara@hotmail.com>
2023-01-05 03:28:32 -0500

    create skill data and change skills/index

---
+++ data/skill-data.js
@@ -0,0 +1,13 @@
+const skills = [
+  {text: 'HTML5', obtained: true, progress: '100%'},
+  {text: 'CSS3', obtained: true, progress: '80%'},
+  {text: 'JavaScript', obtained: true, progress: '75%'},
+  {text: 'MongoDB', obtained: false, progress: '10%'},
+  {text: 'Express', obtained: false, progress: '10%'},
+  {text: 'React', obtained: false, progress: '0%'},
+  {text: 'Node JS', obtained: false, progress: '10%'},
+]
+
+export {
+  skills
+}
